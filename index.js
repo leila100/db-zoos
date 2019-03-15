63c681318c210415566d72dda549ad107bbd424e
Leila <nb.leila10@gmail.com>
2019-03-14 21:26:53 -0500

    Add the post route
    endpoint: /api/zoo
    Returns the id of the new zoo created
    Throws an error if name is empty or doesn't exist

--- index.js
+++ index.js
@@ -2,15 +2,28 @@ const express = require("express")
 const helmet = require("helmet")
 const knex = require("knex")
 const knexConfig = require("./knexfile")
+const db = knex(knexConfig.development)
 
 const server = express()
 
 server.use(express.json())
 server.use(helmet())
 
-const db = knex(knexConfig.development)
-
-// endpoints here
+server.post("/api/zoos", (req, res) => {
+  const name = req.body.name
+  if (!name) {
+    res.status(400).json({ error: "Please give a name to the zoo" })
+  } else {
+    db("zoos")
+      .insert({ name: name })
+      .then(ids => {
+        res.status(201).json(ids[0])
+      })
+      .catch(err => {
+        res.status(500).json(err)
+      })
+  }
+})
 
 const port = 3300
 server.listen(port, function() {
